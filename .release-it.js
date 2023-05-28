6f41f00b0d8641cdcf7b9c3bc15a5189bb6727e5
Chason <cai981012@163.com>
2023-05-28 16:05:30 +0800

    fix: 测试preset

--- .release-it.js
+++ .release-it.js
@@ -3,7 +3,7 @@ module.exports = {
     "@release-it/conventional-changelog": {
       "infile": "CHANGELOG.md",
       "preset": {
-        "name": "eslint",
+        "name": "express",
         "types": [{
           "type": "feat",
           "section": "✨ Features | 新功能",
